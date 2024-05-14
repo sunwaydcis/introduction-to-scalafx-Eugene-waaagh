3c2bca476d50409c58caa8a996a520fa0ea4a20e
eugene <21076765@imail.sunway.edu.my>
2024-05-14 11:51:07 +0800

    Added Primary Stage and Scene

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,10 +1,14 @@
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
 
 object MyApp extends JFXApp {
   stage = new PrimaryStage() {
     title = "First Scala"
-    width = 500
-    height = 500
+
+    scene = new Scene(500, 500) {
+
+    }
   }
+
 }
